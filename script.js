d1253d66cb734d0c74e0d5b20f855c8901ffc9ae
Harshad Sonar <61082703+Harshadsonar@users.noreply.github.com>
2023-06-25 12:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,117 @@
-let n = Math.floor(Math.random() * 5 + 1);
-let main = document.getElementsByTagName('main')[0];
-let images = document.querySelectorAll('img');
-var randomImage = document.getElementsByClassName('none')[0];
-let imageContainer = document.getElementsByClassName("image")[0];
-let arr = [];
-let reset = document.getElementById('reset');
-reset.style.display = 'none';
-let verify = document.getElementById('verify');
-let result = document.getElementsByClassName('para')[0];
-verify.style.display = 'none';
-randomImage.setAttribute("class", `img${n}`);
-images.forEach((e) => {
-    e.addEventListener('click', () => {
-        e.style.border = "8px solid blue";
-        arr.push(e.getAttribute('class'));
-        if (arr.length > 0 && arr.length < 3) {
-            reset.style.display = 'block';
-        }
-        if (arr.length == 2) {
-            verify.style.display = 'block';
-        }
-        if (arr.length > 2) {
-            reset.style.display = 'none';
-            verify.style.display = 'none';
-            let para = document.createElement('p');
-            para.innerText = "Please select only 2 images. Kindly refresh the page";
-            main.append(para);
-        }
-    })
-})
+ 
 
-reset.addEventListener('click', () => {
-    for (let i = 0; i <= arr.length; i++) {
-        arr.pop();
-    }
-    reset.style.display = "none";
-    result.innerText = "";
-    verify.style.display = 'none'
-    images.forEach((e) => {
-        e.style.border = 'none';
-    })
-})
 
-verify.addEventListener('click', () => {
-    if (arr[0] == arr[1]) {
-        result.innerText = 'You are a human. Congratulations!.';
-    } else {
-        result.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
-    }
-})
+const images = Array.from(document.querySelectorAll("img"));
+// const resetButton = document.getElementById("reset");
+// const verifyButton = document.getElementById("verify");
+const messagePara = document.getElementById("para");
+const heading = document.getElementById("h");
 
-imageContainer.append(randomImage);
+
+
+
+// Get a reference to the images container
+const imagesContainer = document.querySelector(".images");
+
+// Create a new image element and set its attributes
+const newImage = document.createElement("img");
+newImage.classList.add("tile", "img6");
+
+// Randomly select an image from the existing array
+const randomIndex = Math.floor(Math.random() * images.length);
+const randomImage = images[randomIndex];
+
+// Set the new image's source to the randomly selected image's source
+newImage.src = randomImage.src;
+
+// Add the new image to the container and the array
+imagesContainer.appendChild(newImage);
+images.push(newImage);
+
+
+
+
+// Get a reference to the reset button and verify button
+const resetButton = document.querySelector("#reset");
+const verifyButton = document.querySelector("#verify");
+
+// Get a reference to the message paragraph
+const message = document.querySelector("#para");
+
+// Initialize variables to keep track of clicked tiles
+let firstClickedTile = null;
+let secondClickedTile = null;
+
+// Add event listeners to each image
+images.forEach(image => {
+image.addEventListener("click", event => {
+// If both tiles have been clicked, return early
+if (firstClickedTile !== null && secondClickedTile !== null) {
+return;
+}
+
+ 
+// Get a reference to the clicked tile
+const clickedTile = event.target;
+
+// If the clicked tile is already selected, return early
+if (clickedTile.classList.contains("selected")) {
+  return;
+}
+
+// If no tiles have been clicked yet, set this tile as the first clicked tile
+if (firstClickedTile === null) {
+  firstClickedTile = clickedTile;
+  firstClickedTile.classList.add("selected");
+  
+  // Show the reset button
+  resetButton.style.display = "inline-block";
+} 
+// If one tile has been clicked, set this tile as the second clicked tile
+else {
+  secondClickedTile = clickedTile;
+  secondClickedTile.classList.add("selected");
+  
+  // Show the verify button
+  verifyButton.style.display = "inline-block";
+}
+});
+});
+
+// Add event listener to reset button
+resetButton.addEventListener("click", () => {
+// Clear clicked tiles and remove selected class from all tiles
+firstClickedTile = null;
+secondClickedTile = null;
+images.forEach(image => image.classList.remove("selected"));
+
+// Hide the reset button and verify button
+resetButton.style.display = "none";
+verifyButton.style.display = "none";
+
+// Clear the message
+message.textContent = "";
+});
+
+// Add event listener to verify button
+verifyButton.addEventListener("click", () => {
+// If both tiles are identical, show success message
+if (firstClickedTile.src === secondClickedTile.src) {
+message.textContent = "You are a human. Congratulations!";
+}
+// If tiles are not identical, show error message
+else {
+message.textContent = "We can't verify you as a human. You selected the non-identical tiles.";
+}
+
+// Remove selected class from all tiles
+images.forEach(image => image.classList.remove("selected"));
+
+// Hide the reset button and verify button
+resetButton.style.display = "none";
+verifyButton.style.display = "none";
+
+// Clear clicked tiles
+firstClickedTile = null;
+secondClickedTile = null;
+});
